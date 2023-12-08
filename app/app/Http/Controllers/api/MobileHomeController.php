baf632c429583dd9fb41427874e6cc1e202a5cd5
kawasaki <tmkhornymrokyu@gmail.com>
2023-12-08 17:42:46 +0900

    apiのカラム追加など

--- app/app/Http/Controllers/api/MobileHomeController.php
+++ app/app/Http/Controllers/api/MobileHomeController.php
@@ -9,14 +9,22 @@ use App\Models\User;
 
 class MobileHomeController extends Controller
 {
-    //
-    public function index()
+    public function index(Request $request)
     {
-        $accessToken = Auth::user()->createToken('TokenName')->accessToken;
+        // トークンを取得
+        $accessToken = $request->bearerToken();
+
+        // トークンがない場合やユーザーが見つからない場合はエラーレスポンスを返す
+        if (!$accessToken || !$user = Auth::guard('api')->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        // ここで必要な処理を実行する（例えば、他のデータを返したり、処理を実行したり）
 
         return response()->json([
             'message' => 'Welcome to the API',
-            'access_token' => $accessToken,
+            'user_id' => $user->id,
+            'email' => $user->email,
         ]);
     }
 }
