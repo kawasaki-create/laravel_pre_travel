20d06905251281c01bcbe8211521f5f4d27722a8
kawasaki <r.kawasaki@leafnet.jp>
2024-03-29 15:41:07 +0900

    AndroidとiOSで登録時の呼び出しクラス変更(動作未確認のため本番プル絶対NG)

--- app/app/Http/Controllers/Auth/RegisterController.php
+++ app/app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            // 以下Web版の登録
+            'register_os' => 0,
         ]);
     }
 }
